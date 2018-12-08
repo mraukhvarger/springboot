4eaa12c2bd3eaf1f97e8b495a765d2695002ce23
Maxim Raukhvarger <raukhvarger@gmail.com>
2018-12-08 06:13:23 +0300

    fill db on startup

--- src/main/java/com/ystu/main/WebServiceConfig.java
+++ src/main/java/com/ystu/main/WebServiceConfig.java
@@ -1,10 +1,23 @@
 package com.ystu.main;
 
 
+import com.ystu.entities.User;
+import com.ystu.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 
 
 @Configuration
 public class WebServiceConfig {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @EventListener(ApplicationReadyEvent.class)
+    void fillDatabaseOnStartup() {
+        userRepository.save(new User("user", "password"));
+        userRepository.save(new User("abcd", "1234"));
+    }
 }
